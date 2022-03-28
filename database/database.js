b7b463fdb7f00e7f57898d22205e80dbc25f18c3
carvalhodaniel <carvalhodanielg@gmail.com>
2022-03-28 20:38:54 -0300

    teste

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('qa','root','password',{
-    host: 'localhost',
-    dialect: 'mysql'
-})//qa é o  bd criado no mysqle
-
-module.exports = connection;
